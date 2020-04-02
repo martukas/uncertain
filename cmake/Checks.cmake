f4ae06972bf433b70f2c86c79d1c7181a068ce55
Martin Shetty <martukas@gmail.com>
2020-04-02 16:33:17 -0400

    "added clang-tidy checks; updates #6"

--- cmake/Checks.cmake
+++ cmake/Checks.cmake
@@ -1,76 +1,45 @@
 # ------------------------------------------------------------------------------
-# Clang Tidy
-# ------------------------------------------------------------------------------
-
-if(ENABLE_CLANG_TIDY)
-
-    find_program(CLANG_TIDY_BIN clang-tidy-4.0)
-    find_program(RUN_CLANG_TIDY_BIN run-clang-tidy-4.0.py)
-
-    if(CLANG_TIDY_BIN STREQUAL "CLANG_TIDY_BIN-NOTFOUND")
-        message(FATAL_ERROR "unable to locate clang-tidy-4.0")
-    endif()
-
-    if(RUN_CLANG_TIDY_BIN STREQUAL "RUN_CLANG_TIDY_BIN-NOTFOUND")
-        message(FATAL_ERROR "unable to locate run-clang-tidy-4.0.py")
-    endif()
-
-    list(APPEND RUN_CLANG_TIDY_BIN_ARGS
-        -clang-tidy-binary ${CLANG_TIDY_BIN}
-        -header-filter=.*
-        -checks=clan*,cert*,misc*,perf*,cppc*,read*,mode*,-cert-err58-cpp,-misc-noexcept-move-constructor
-    )
-
-    add_custom_target(
-        tidy
-        COMMAND ${RUN_CLANG_TIDY_BIN} ${RUN_CLANG_TIDY_BIN_ARGS}
-        COMMENT "running clang tidy"
-    )
-
-endif()
-
-# ------------------------------------------------------------------------------
 # CppCheck
 # ------------------------------------------------------------------------------
 
 if(ENABLE_CPPCHECK)
 
-    list(APPEND CPPCHECK_CMAKE_ARGS
-        "-DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}"
+  list(APPEND CPPCHECK_CMAKE_ARGS
+    "-DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}"
     )
 
-    ExternalProject_Add(
-        cppcheck
-        GIT_REPOSITORY      https://github.com/danmar/cppcheck.git
-        GIT_TAG             1.79
-        GIT_SHALLOW         1
-        CMAKE_ARGS          ${CPPCHECK_CMAKE_ARGS}
-        PREFIX              ${CMAKE_BINARY_DIR}/external/cppcheck/prefix
-        TMP_DIR             ${CMAKE_BINARY_DIR}/external/cppcheck/tmp
-        STAMP_DIR           ${CMAKE_BINARY_DIR}/external/cppcheck/stamp
-        DOWNLOAD_DIR        ${CMAKE_BINARY_DIR}/external/cppcheck/download
-        SOURCE_DIR          ${CMAKE_BINARY_DIR}/external/cppcheck/src
-        BINARY_DIR          ${CMAKE_BINARY_DIR}/external/cppcheck/build
+  externalproject_add(
+    cppcheck
+    GIT_REPOSITORY https://github.com/danmar/cppcheck.git
+    GIT_TAG 1.79
+    GIT_SHALLOW 1
+    CMAKE_ARGS ${CPPCHECK_CMAKE_ARGS}
+    PREFIX ${CMAKE_BINARY_DIR}/external/cppcheck/prefix
+    TMP_DIR ${CMAKE_BINARY_DIR}/external/cppcheck/tmp
+    STAMP_DIR ${CMAKE_BINARY_DIR}/external/cppcheck/stamp
+    DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/external/cppcheck/download
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/external/cppcheck/src
+    BINARY_DIR ${CMAKE_BINARY_DIR}/external/cppcheck/build
+  )
+
+  list(APPEND CPPCHECK_ARGS
+    --enable=warning,style,performance,portability,unusedFunction
+    --std=c++11
+    --verbose
+    --error-exitcode=1
+    --language=c++
+    -DMAIN=main
+    -I ${CMAKE_SOURCE_DIR}/include
+    ${CMAKE_SOURCE_DIR}/include/*.h
+    ${CMAKE_SOURCE_DIR}/src/*.cpp
+    ${CMAKE_SOURCE_DIR}/test/*.cpp
     )
 
-    list(APPEND CPPCHECK_ARGS
-        --enable=warning,style,performance,portability,unusedFunction
-        --std=c++11
-        --verbose
-        --error-exitcode=1
-        --language=c++
-        -DMAIN=main
-        -I ${CMAKE_SOURCE_DIR}/include
-        ${CMAKE_SOURCE_DIR}/include/*.h
-        ${CMAKE_SOURCE_DIR}/src/*.cpp
-        ${CMAKE_SOURCE_DIR}/test/*.cpp
-    )
-
-    add_custom_target(
-        check
-        COMMAND ${CMAKE_BINARY_DIR}/bin/cppcheck ${CPPCHECK_ARGS}
-        COMMENT "running cppcheck"
-    )
+  add_custom_target(
+    check
+    COMMAND ${CMAKE_BINARY_DIR}/bin/cppcheck ${CPPCHECK_ARGS}
+    COMMENT "running cppcheck"
+  )
 
 endif()
 
@@ -79,22 +48,22 @@ endif()
 # ------------------------------------------------------------------------------
 
 if(ENABLE_ASAN)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O1")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fuse-ld=gold")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-omit-frame-pointer")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=leak")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O1")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fuse-ld=gold")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-omit-frame-pointer")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=leak")
 endif()
 
 if(ENABLE_USAN)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fuse-ld=gold")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=undefined")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fuse-ld=gold")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=undefined")
 endif()
 
 if(ENABLE_TSAN)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fuse-ld=gold")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=thread")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fuse-ld=gold")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=thread")
 endif()
 
 # ------------------------------------------------------------------------------
